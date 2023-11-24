d1aaeb953c068c7a4ebfbfdbf133b4c1149d5a8c
Amar Diyab <diyabamar3043@gmail.com>
2023-11-24 11:23:53 +0530

    Styled Footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
   return (
     <div className='bg-[#0d3d4e] pt-[2rem] pb-5 flex flex-col space-y-3 '>
         <Navbar />
-        <hr />
-        <div className='flex justify-between mx-[10rem] mt-5 text-[#5c9db4]'>
-            <span>Copyright</span>
+        <div className='mx-[10rem] bg-[#5c9db4] h-[1px]   ' />
+        <div className='flex justify-between mx-[10rem] mt-10 text-[#5c9db4]'>
+            <span className='font-extralight text-sm'>Copyright </span>
             <div className='flex gap-5'>
             <FaFacebook />
             <GrInstagram />
