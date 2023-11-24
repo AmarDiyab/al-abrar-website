498fe9064077498d318776ad202110e42ecd8f83
Amar Diyab <diyabamar3043@gmail.com>
2023-11-24 11:30:08 +0530

    Added Text Overlay in Gallery

--- app/components/Gallery.jsx
+++ app/components/Gallery.jsx
@@ -13,32 +13,71 @@ const Gallery = () => {
       <span className="text-5xl font-semibold ">Completed Journey</span>
 
       <div className="bg-[#f0eded] -mx-[10rem] mt-[15rem]  ">
+        <div className="grid grid-cols-3 gap-4 -my-[10rem] mx-[10rem] text-white  ">
+          <div className="col-span-1 relative">
+            <Image
+              src={img1}
+              alt="First Image"
+              className="w-full h-[350px] rounded-2xl "
+            />
+            <div className="absolute flex flex-col bottom-5 left-5 ">
+              <span className="text-3xl font-semibold">Rome</span>
+              <span className="font-extralight">543 user travel here</span>
+            </div>
+          </div>
+          <div className="col-span-2 relative">
+            <Image
+              src={img2}
+              alt="Second Image"
+              className="w-full h-[350px] rounded-2xl "
+            />
+             <div className="absolute flex flex-col bottom-5 left-5 ">
+              <span className="text-3xl font-semibold">Milan</span>
+              <span className="font-extralight">543 user travel here</span>
+            </div>
+          </div>
 
-      <div className="grid grid-cols-3 gap-4 -my-[10rem] mx-[10rem]  ">
-        <div className="col-span-1">
-          <Image src={img1} alt="First Image" className="w-full h-[350px] rounded-2xl " />
-        </div>
-        <div className="col-span-2">
-          <Image src={img2} alt="Second Image" className="w-full h-[350px] rounded-2xl " />
+          <div className="col-span-1 relative">
+            <Image
+              src={img3}
+              alt="Third Image"
+              className="w-full h-[350px] rounded-2xl"
+            />
+             <div className="absolute flex flex-col bottom-5 left-5 ">
+              <span className="text-3xl font-semibold">Raja Ampat</span>
+              <span className="font-extralight">53 user travel here</span>
+            </div>
+          </div>
+          <div className="col-span-1 relative">
+            <Image
+              src={img4}
+              alt="Fourth Image"
+              className="w-full h-[350px] rounded-2xl"
+            />
+             <div className="absolute flex flex-col bottom-5 left-5 ">
+              <span className="text-3xl font-semibold">Tiga Warna</span>
+              <span className="font-extralight">513 user travel here</span>
+            </div>
+          </div>
+          <div className="col-span-1 relative ">
+            <Image
+              src={img5}
+              alt="Fifth Image"
+              className="w-full h-[350px] rounded-2xl "
+            />
+             <div className="absolute flex flex-col bottom-5 left-5 ">
+              <span className="text-3xl font-semibold">Sempu Beach</span>
+              <span className="font-extralight">213 user travel here</span>
+            </div>
+          </div>
         </div>
 
-        <div className="col-span-1">
-          <Image src={img3} alt="Third Image" className="w-full h-[350px] rounded-2xl" />
-        </div>
-        <div className="col-span-1">
-          <Image src={img4} alt="Fourth Image" className="w-full h-[350px] rounded-2xl" />
-        </div>
-        <div className="col-span-1 group overflow-hidden ">
-          <Image src={img5} alt="Fifth Image" className="w-full h-[350px] rounded-2xl " />
+        <div className="flex justify-center mx-[10rem] mt-[12rem] mb-20 ">
+          <button className="bg-[#d9ed82] text-[#0d3d4e] px-16 py-3 rounded-full font-semibold ">
+            More Journey
+          </button>
         </div>
       </div>
-
-      <div className="flex justify-center mx-[10rem] mt-[12rem] mb-20 ">
-        <button className="bg-[#d9ed82] text-[#0d3d4e] px-16 py-3 rounded-full font-semibold ">More Journey</button>
-      </div>
-
-      </div>
-
     </div>
   );
 };
