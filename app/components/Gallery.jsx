e2dae6e9d3f53914390059bc101153d4a00ded32
Amar Diyab <diyabamar3043@gmail.com>
2023-12-05 15:19:31 +0530

    Added hover effect for Gallery images

--- app/components/Gallery.jsx
+++ app/components/Gallery.jsx
@@ -14,11 +14,11 @@ const Gallery = () => {
 
       <div className="bg-[#f0eded] -mx-[10rem] mt-[15rem]  ">
         <div className="grid grid-cols-3 sm:grid-cols-1 gap-4 sm:gap-10 -my-[10rem] mx-[10rem] text-white  ">
-          <div className="col-span-1 relative cursor-pointer ">
+          <div className="col-span-1 relative cursor-pointer group overflow-hidden rounded-2xl ">
             <Image
               src={img1}
               alt="First Image"
-              className="w-full h-[350px] sm:h-[300px] object-cover rounded-2xl "
+              className="w-full h-[350px] sm:h-[300px] object-cover rounded-2xl group-hover:scale-125 duration-300 "
             />
             <div className="absolute flex flex-col bottom-5 left-5 z-20 ">
               <h1 className="text-3xl font-semibold sm:text-left ">Rome</h1>
@@ -27,11 +27,11 @@ const Gallery = () => {
             <div className="absolute bottom-0 w-full bg-gradient-to-t from-[#44616b] to-transparent  h-[200px] z-10 rounded-2xl  "></div>
           </div>
 
-          <div className="col-span-2 sm:col-span-1 relative cursor-pointer">
+          <div className="col-span-2 sm:col-span-1 relative cursor-pointer group overflow-hidden rounded-2xl">
             <Image
               src={img2}
               alt="Second Image"
-              className="w-full h-[350px] sm:h-[300px] rounded-2xl "
+              className="w-full h-[350px] sm:h-[300px] rounded-2xl group-hover:scale-125 duration-300 "
             />
              <div className="absolute flex flex-col bottom-5 left-5 z-20 ">
               <h1 className="text-3xl font-semibold sm:text-left">Milan</h1>
@@ -40,11 +40,11 @@ const Gallery = () => {
             <div className="absolute bottom-0 w-full bg-gradient-to-t from-[#44616b] to-transparent  h-[200px] z-10 rounded-2xl  "></div>
           </div>
 
-          <div className="col-span-1 relative cursor-pointer">
+          <div className="col-span-1 relative cursor-pointer group overflow-hidden rounded-2xl">
             <Image
               src={img3}
               alt="Third Image"
-              className="w-full h-[350px] sm:h-[300px] rounded-2xl"
+              className="w-full h-[350px] sm:h-[300px] rounded-2xl group-hover:scale-125 duration-300"
             />
              <div className="absolute flex flex-col bottom-5 left-5 z-20 ">
               <h1 className="text-3xl font-semibold sm:text-left">Raja Ampat</h1>
@@ -53,11 +53,11 @@ const Gallery = () => {
             <div className="absolute bottom-0 w-full bg-gradient-to-t from-[#44616b] to-transparent  h-[200px] z-10 rounded-2xl  "></div>
           </div>
 
-          <div className="col-span-1 relative cursor-pointer">
+          <div className="col-span-1 relative cursor-pointer group overflow-hidden rounded-2xl">
             <Image
               src={img4}
               alt="Fourth Image"
-              className="w-full h-[350px] sm:h-[300px] rounded-2xl"
+              className="w-full h-[350px] sm:h-[300px] rounded-2xl group-hover:scale-125 duration-300"
             />
              <div className="absolute flex flex-col bottom-5 left-5 z-20 ">
               <h1 className="text-3xl font-semibold sm:text-left">Tiga Warna</h1>
@@ -66,11 +66,11 @@ const Gallery = () => {
             <div className="absolute bottom-0 w-full bg-gradient-to-t from-[#44616b] to-transparent  h-[200px] z-10 rounded-2xl  "></div>
           </div>
 
-          <div className="col-span-1 relative cursor-pointer ">
+          <div className="col-span-1 relative cursor-pointer group overflow-hidden rounded-2xl ">
             <Image
               src={img5}
               alt="Fifth Image"
-              className="w-full h-[350px] sm:h-[300px] rounded-2xl "
+              className="w-full h-[350px] sm:h-[300px] rounded-2xl group-hover:scale-125 duration-300"
             />
              <div className="absolute flex flex-col bottom-5 left-5 z-20">
               <h1 className="text-3xl font-semibold">Sempu Beach</h1>
